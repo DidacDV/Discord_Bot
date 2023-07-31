54f724fb4a29c48f898b833eb2a49ef78be9d1c5
DidacDV <113896749+DidacDV@users.noreply.github.com>
2023-07-31 20:47:45 +0200

    Update bot.py
    added cat command

--- bot/bot.py
+++ bot/bot.py
@@ -1,23 +1,24 @@
 import discord 
 import os
+import aiohttp
 from dotenv import load_dotenv
 from discord.ext import commands
 #bot 29/07/2023
 
-bot_test = commands.Bot(command_prefix= '.!', intents=discord.Intents.all())  #What prefix will the bot use to execute its commands + getting all intents
+bot_test = commands.Bot(command_prefix= '.!', intents=discord.Intents.all())   #prefix for the bot use to execute its commands + getting all intents
 load_dotenv()
 token = os.getenv("TOKEN")  #GETS TOKEN FROM ENV FILE
 
 
 @bot_test.event
-async def on_ready():       #Coroutine
-    print("Ready to start!")                    #OUTPUT FOR COMMAND LINE
+async def on_ready():       
+    print("Ready to start!")                    
     print(f"Bot discord tag = {bot_test.user}")
 
 @bot_test.event
 async def on_guild_join(guild):         #EXECUTES WHEN JOINING SERVER(GUILD)
     for channel in guild.text_channels:
-        if channel.permissions_for(guild.me):                              #LOOKS FOR CHANNEL TO PRINT WHEN JOINING
+        if channel.permissions_for(guild.me):                              #looks for channel to print
             desc = "This is a test bot by [DidacDV](https://github.com/DidacDV)\n\nMy **prefix** for commands is **.!**\n\nType **.!help** to see all commands"
             embed = discord.Embed(title= "Kei_bot",color= discord.Color.dark_gold())
             embed.description = desc
@@ -26,7 +27,13 @@ async def on_guild_join(guild):         #EXECUTES WHEN JOINING SERVER(GUILD)
             break
 
 @bot_test.command()
-async def test(ctx):        #ctx = Context object
-    await ctx.send("test")
- 
+async def cat(ctx):        #CAT COMMAND
+    async with aiohttp.ClientSession() as session:  #creates session
+        async with session.get("https://api.thecatapi.com/v1/images/search") as resp:   #gets response from catapi
+            if resp.status == 200:     #checks if response is correct
+                resp = await resp.json()
+                resp = resp[0]['url']
+                await ctx.send(resp)        #sends URL that appears as cat image
+
+
 bot_test.run(token) 
